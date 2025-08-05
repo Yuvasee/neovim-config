d1387ec88d74f0eec40fe9a8d049e46c885cad9d
v9v <yuri.v9v@gmail.com>
2025-08-05 22:42:49 +0300

    Tabs indents in prettier

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters = {
+      prettier = {
+        prepend_args = { "--use-tabs", "--tab-width", "2" },
+      },
+    },
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      vue = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      less = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      ["markdown.mdx"] = { "prettier" },
+      graphql = { "prettier" },
+      handlebars = { "prettier" },
+    },
+  },
+}
